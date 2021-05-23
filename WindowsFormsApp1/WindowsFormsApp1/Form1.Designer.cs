0d992927ebd1b735271ed3e8a593699e7130f34b
IEEE BAUN <57831953+ieeebaunsb@users.noreply.github.com>
2021-05-23 18:45:11 +0300

    İnstagram Cekilis Botu

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(191, 133);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(762, 135);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ayarlar";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.DarkOrange;
+            this.textBox1.Location = new System.Drawing.Point(123, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(633, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(29, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Gönderi Linki";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.DarkOrange;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.comboBox1.Location = new System.Drawing.Point(123, 56);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(7, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Kazanacak Kişi Sayısı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(373, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Yedek Sayısı";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.BackColor = System.Drawing.Color.DarkOrange;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.comboBox2.Location = new System.Drawing.Point(458, 56);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.Location = new System.Drawing.Point(194, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(385, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Çekilişi Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Location = new System.Drawing.Point(191, 292);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(272, 235);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Katılımcılar";
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.DarkOrange;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(10, 19);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(256, 199);
+            this.listBox1.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.listBox2);
+            this.groupBox3.Location = new System.Drawing.Point(584, 292);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(321, 105);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Kazananlar";
+            this.groupBox3.Enter += new System.EventHandler(this.groupBox3_Enter);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.listBox3);
+            this.groupBox4.Location = new System.Drawing.Point(584, 422);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(321, 118);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Yedekler";
+            // 
+            // listBox2
+            // 
+            this.listBox2.BackColor = System.Drawing.Color.DarkOrange;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(6, 17);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(315, 82);
+            this.listBox2.TabIndex = 0;
+            // 
+            // listBox3
+            // 
+            this.listBox3.BackColor = System.Drawing.Color.DarkOrange;
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(6, 23);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(315, 82);
+            this.listBox3.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button2.Location = new System.Drawing.Point(201, 533);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(262, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Dışa Aktar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button3.Location = new System.Drawing.Point(617, 397);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(262, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Dışa Aktar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button4.Location = new System.Drawing.Point(617, 535);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(262, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Dışa Aktar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Coral;
+            this.label4.Location = new System.Drawing.Point(97, 33);
+            this.label4.MaximumSize = new System.Drawing.Size(1000, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(935, 55);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "IEEE BAUN INSTAGRAM ÇEKİLİŞ ARACI";
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.ımageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(1131, 600);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.ForeColor = System.Drawing.Color.DarkOrange;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ImageList ımageList1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,197 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox3_Enter(object sender,     EventArgs e)
+        {
+
+        }
+        public IWebDriver driver { get; set; }
+        public async void DriverBaslat(string proxy)
+        {
+            ChromeDriverService chromeDriverService = ChromeDriverService.CreateDefaultService();
+            chromeDriverService.HideCommandPromptWindow = true;
+            ChromeOptions chromeOptions = new ChromeOptions();
+            if (!string.IsNullOrEmpty(proxy))
+            {
+                chromeOptions.AddArgument("--proxy-server=" + proxy);
+            }
+            chromeOptions.AddExcludedArgument("enable-automation");
+            chromeOptions.AddAdditionalCapability("useAutomationExtension", false);
+            chromeOptions.AddArguments("--allow-file-access");
+            chromeOptions.AddArgument("--disable-web-security");
+            chromeOptions.AddArgument("--allow-running-insecure-content");
+            driver = new ChromeDriver(chromeDriverService, chromeOptions, TimeSpan.FromMinutes(30.0));
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(10);
+            await Task.Delay(2000);
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() =>
+            {
+                try
+                {
+                    DriverBaslat("");
+                    driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(59);
+                    driver.Navigate().GoToUrl(textBox1.Text);
+                    var carpi = driver.FindElement(By.ClassName("xqRnw"));
+                    carpi.Click();
+                    IList<IWebElement> yorumlar = driver.FindElements(By.ClassName("gElp9"));
+
+
+                    if (yorumlar.Count > 11)
+                    {
+                        string xpath = "//*[@id='react-root']/section/main/div/div[1]/article/div[3]/div[1]/ul/li/div/button";
+                        var loadMorebtn = driver.FindElement(By.XPath(xpath));
+
+
+
+
+                        while (loadMorebtn.Displayed)
+                        {
+
+                            loadMorebtn.Click();
+                            try
+                            {
+                                loadMorebtn = driver.FindElement(By.XPath(xpath));
+                            }
+                            catch (Exception ex)
+                            {
+
+                                break;
+                            }
+                            Thread.Sleep(1000);
+
+
+                        }
+                    }
+
+
+
+
+
+                     yorumlar = driver.FindElements(By.ClassName("gElp9"));
+
+                    foreach (var item in yorumlar)
+                    {
+                        var container = item.FindElement(By.ClassName("C4VMK"));
+                        var kullaniciAdi = container.FindElement(By.ClassName("_6lAjh")).Text;
+                        listBox1.Items.Add(kullaniciAdi);
+                    }
+                    listBox1.Items.RemoveAt(0);
+                    
+                    driver.Dispose();
+                    driver.Quit();
+                    
+
+                    int kazananKisiSayisi = Convert.ToInt32(comboBox1.SelectedItem);
+                    int yedekKisiSayisi = Convert.ToInt32(comboBox2.SelectedItem);
+
+                    for (int i = 0; i < kazananKisiSayisi; i++)
+                    {
+                        int cikanKisi = rnd.Next(0, listBox1.Items.Count);
+                        listBox2.Items.Add(listBox1.Items[cikanKisi]);
+                        listBox1.Items.RemoveAt(cikanKisi);
+
+                    }
+                    for (int i = 0; i < yedekKisiSayisi; i++)
+                    {
+                        int cikanKisi = rnd.Next(0, listBox1.Items.Count);
+                        listBox3.Items.Add(listBox1.Items[cikanKisi]);
+                        listBox1.Items.RemoveAt(cikanKisi);
+
+                    }
+
+
+
+                }
+                catch (Exception x)
+                {
+
+                    MessageBox.Show(x.Message);
+                }
+
+            });
+
+        }
+
+        Random rnd;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            rnd = new Random();
+
+        }
+
+        public void DosyaKaydet(ListBox lst, string adi)
+        {
+            try
+            {
+                string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\" + adi;
+                using (StreamWriter sw= new StreamWriter(path))
+                {
+                    foreach(var item in lst.Items)
+                    {
+                        sw.WriteLine(item.ToString());
+                    }
+                    sw.Close();
+                    MessageBox.Show("Masaüstüne \""+adi+" \" olarak kayıt edildi");
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DosyaKaydet(listBox1, "Katılımcılar.txt");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DosyaKaydet(listBox2, "Kazananlar.txt");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DosyaKaydet(listBox3, "Yedekler.txt");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
